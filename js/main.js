f4fd3db87fcd60f10e4fd6b0c049ecd6ee1e9c27
choidayeon <41932112+choidayeon@users.noreply.github.com>
2018-11-21 18:19:49 +0900

    slide

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+	$("ul.sub").hide();
+	$("#lnb>ul>li").mouseenter(function(){
+		var i = $(this).index();
+		$("#lnb>ul>li:eq("+i+") ul").slideDown();
+	}).mouseleave(function(){
+		$("ul.sub").hide();
+	});
+});
