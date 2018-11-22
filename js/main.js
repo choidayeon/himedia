5b1b2a58683b58348dd814665791b9c5cc54e184
choidayeon <41932112+choidayeon@users.noreply.github.com>
2018-11-22 17:14:26 +0900

    top

--- js/main.js
+++ js/main.js
@@ -7,5 +7,14 @@ $(function(){
 		$("ul.sub").hide();
 	});
 
-	$(".slider_wrap").bxSlider();
+	$(".slider_wrap").bxSlider({
+		auto:true
+	});
+
+
+	$(".top").click(function(){
+		// window.scrollTo(0,0)
+		// $("html,body").scrollTop(0);
+		$("html,body").stop().animate({scrollTop:0},500);
+	});	
 });
